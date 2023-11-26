980811c085043bca470f1ef566c6d3f4bbbecb84
Yoshida Sho <s22C1137ST@s.chibakoudai.jp>
2023-11-26 14:46:12 +0900

    test

--- test.bash
+++ test.bash
@@ -16,7 +16,8 @@ out=$(seq 1 2 5 | ./plus)
 [ "${out}" = "合計: 9.0, 総乗: 15.0" ] || ng $LINENO
 
 out=$(echo abc | ./plus)
-[ "${out}" = "数値以外のものが入力されています: abc$'\n'合計: 0, 総乗: 1" ] || ng $LINENO
+[ "${out}" = "数値以外のものが入力されています: abc
+合計: 0, 総乗: 1" ] || ng $LINENO
 　
 [ "$res" = 0 ] && echo OK
 exit $res
