19bad8d82ecb8777e8e5075ffc5162887102537e
Yoshida Sho <s22C1137ST@s.chibakoudai.jp>
2023-11-26 14:11:26 +0900

    TEST

--- test.bash
+++ test.bash
@@ -1,3 +1,19 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2023 Sho Yoshida　　　　　
+# SPDX-License-Identifier: BSD-3-Clause
+ng () {
+      echo NG at Line $1
+      res=1
+}
+　
+res=0
+　
+### I/O TEST ###
+out=$(seq 5 | ./plus)
+[ "${out}" = "合計:15.0, 総乗:120.0"  ] || ng ${LINENO}
 
-seq 5 | ./plus
+out=$(seq 1 2 5 | ./plus)
+[ "${out}" = "合計:9, 総乗:45" ] || ng $LINENO
+　
+[ "$res" = 0 ] && echo OK
+exit $res
