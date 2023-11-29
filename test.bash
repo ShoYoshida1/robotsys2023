9a31f34fea5414c11ccec366b8fbeebf538a0e7b
Yoshida Sho <s22C1137ST@s.chibakoudai.jp>
2023-11-29 09:42:58 +0900

    update code and add test pattern

--- test.bash
+++ test.bash
@@ -16,5 +16,21 @@ out=$(seq 5 | ./plus)
 out=$(seq 50 | ./plus)
 [ "${out}" = "合計: 1275, 総乗: 30414093201713378043612608166064768844377641568960512000000000000"  ] || ng ${LINENO}
 
+out=$(seq 1 2 5 | ./plus)
+[ "${out}" = "合計: 9, 総乗: 15"  ] || ng ${LINENO}
+
+out=$(seq -1 2 3 | ./plus)
+[ "${out}" = "合計: 3, 総乗: -3"  ] || ng ${LINENO}
+
+out=$(seq 0.1 0.2 0.5 | ./plus)
+[ "${out}" = "合計: 0.9, 総乗: 0.015"  ] || ng ${LINENO}
+
+out=$(seq -0.1 0.2 0.3 | ./plus)
+[ "${out}" = "合計: 0.3, 総乗: -0.003"  ] || ng ${LINENO}
+### STRANGE INPUT ###
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
 [ "$res" = 0 ] && echo OK
 exit $res
