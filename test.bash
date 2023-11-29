3439e6fc9ff5cc1bba2110fea57aa9d632c6a542
Yoshida Sho <s22C1137ST@s.chibakoudai.jp>
2023-11-29 11:14:38 +0900

    retest5

--- test.bash
+++ test.bash
@@ -38,21 +38,21 @@ out=$(seq 0.02 0.02 0.1 | ./plus)
 [ "${out}" = "合計: 0.30, 総乗: 3.840E-7"  ] || ng ${LINENO}
 
 ### STRANGE INPUT ###
-out=$(echo | ./plus)
-[ "$?" = 1 ] || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
-
-out=$(echo 1a2b3c| ./plus)
-[ "$?" = 1 ] || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
-
 out=$(echo あ | ./plus)
-[ "$?" = 1 ] || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+[ "$?" = 1 ] || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
+
+out=$(echo  | ./plus)
+[ "$?" = 1 ] || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
 
 out=$(echo abc | ./plus)
-[ "$?" = 1 ] || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+[ "$?" = 1 ] || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
+
+out=$(echo 1a2b3c | ./plus)
+[ "$?" = 1 ] || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
 
 [ "$res" = 0 ] && echo OK
 
