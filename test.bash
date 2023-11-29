9cef8a2bfe8958e363e129257c30bf428aa07ae5
Yoshida Sho <s22C1137ST@s.chibakoudai.jp>
2023-11-29 10:04:47 +0900

    retest2

--- test.bash
+++ test.bash
@@ -27,10 +27,20 @@ out=$(seq 0.1 0.2 0.5 | ./plus)
 
 out=$(seq -0.1 0.2 0.3 | ./plus)
 [ "${out}" = "合計: 0.3, 総乗: -0.003"  ] || ng ${LINENO}
+
 ### STRANGE INPUT ###
 out=$(echo | ./plus)
 [ "$?" = 1 ] || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo abc | ./plus)
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
 [ "$res" = 0 ] && echo OK
+
 exit $res
